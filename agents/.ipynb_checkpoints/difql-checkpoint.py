d8b7e8aa4956048dd34c4e9e75ce5c65b8cf7852
Xyu Chern <nakupenda.ics@gmail.com>
2025-09-07 23:44:44 +0800

    fastdifql

--- agents/.ipynb_checkpoints/difql-checkpoint.py
+++ agents/.ipynb_checkpoints/difql-checkpoint.py
@@ -37,7 +37,7 @@ class DIFQLAgent(DOALAgent,IFQLAgent):
         vel = x_1 - x_0
 
         pred = self.network.select('actor_flow')(batch['observations'], x_t, t, params=grad_params)
-        actor_loss = jnp.mean((pred - vel) ** 2)
+        actor_loss = self.config['alpha_actor'] *  jnp.mean((pred - vel) ** 2)
 
         return actor_loss, {
             'actor_loss': actor_loss,
