0cf957eed94fc69d7fc49c1bfceac89da421165e
Xyu Chern <nakupenda.ics@gmail.com>
2025-09-09 03:25:00 +0800

    run alpha with solver

--- scripts/run_alpha.sh
+++ scripts/run_alpha.sh
@@ -6,7 +6,7 @@
 
 # Check for the correct number of arguments
 if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
-    echo "Usage: $0 <agent_name> <env_name> [exp_name]"
+    echo "Usage: $0 <agent_name> <env_name> [solver]"
     echo "Example: $0 my_new_agent my_new_environment-v1 my_experiment"
     exit 1
 fi
@@ -16,20 +16,20 @@ AGENT_NAME=$1
 ENV_NAME=$2
 
 # Check if the third argument exists and assign it
-EXP_NAME=""
+SOLVER="diag_hess"
 if [ "$#" -eq 3 ]; then
-    EXP_NAME=$3
+    SOLVER=$3
 fi
 
 # Define the list of alpha parameters
-alphas=(  10.0 30 100 300 1000.0 3000)
+alphas=(  3 30 300 )
 
 # Loop through all alpha values
 for alpha in "${alphas[@]}"; do
-    echo "Running with Agent: $AGENT_NAME, Env: $ENV_NAME, Alpha: $alpha, ExpName: $EXP_NAME"
+    echo "Running with Agent: $AGENT_NAME, Env: $ENV_NAME, Alpha: $alpha, SOLVER: $SOLVER"
     python main.py \
         --agent_name "$AGENT_NAME" \
         --env_name "$ENV_NAME" \
         --alpha "$alpha" \
-        --exp_name "$EXP_NAME"
+        --solver "$SOLVER"
 done
