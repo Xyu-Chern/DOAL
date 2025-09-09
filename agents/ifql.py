27d1dcf4c514a2e641e04b5687647a492a8ef97c
Xyu Chern <nakupenda.ics@gmail.com>
2025-09-09 11:50:07 +0800

    iql gn

--- agents/ifql.py
+++ agents/ifql.py
@@ -226,7 +226,7 @@ class IFQLAgent(flax.struct.PyTreeNode):
 
         network_def = ModuleDict(networks)
         network_tx = optax.chain(
-      #      optax.clip_by_global_norm(max_norm=config["gn"]),
+            optax.clip_by_global_norm(max_norm=config["gn"]),
             optax.adam(learning_rate=config['lr'])
         )
         network_params = network_def.init(init_rng, **network_args)['params']
