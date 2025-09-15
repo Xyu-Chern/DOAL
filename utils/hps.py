6447ae11b73c44247e023210d7246bdfbe03a033
Xyu Chern <nakupenda.ics@gmail.com>
2025-09-16 00:55:40 +0800

    update d4rl hps

--- utils/hps.py
+++ utils/hps.py
@@ -235,258 +235,86 @@ hyperparameters ={
             "alpha_critic": 0.0
         }
     },
-    "pen-human-v1": {
-        "fql": {
-            "alpha_actor": 10000.0
-        },
-        "trigflow": {
-            "alpha_actor": 10000.0
-        },
-        "rebrac": {
-            "alpha_actor": 0.1
-        },
-        "alpha_critic": 0.5,
-        "alpha": 0.1,
-        "drebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.5
-        }
-    },
-    "pen-cloned-v1": {
-        "fql": {
-            "alpha_actor": 10000.0
-        },
-        "trigflow": {
-            "alpha_actor": 10000.0
-        },
-        "rebrac": {
-            "alpha_actor": 0.05
-        },
-        "alpha_critic": 0.5,
-        "alpha": 0.05,
-        "drebrac": {
-            "alpha_actor": 0.05,
-            "alpha_critic": 0.5
-        }
-    },
     "pen-expert-v1": {
-        "alpha_actor": 3000.0,
-        "rebrac": {
-            "alpha_actor": 0.01
-        },
-        "alpha_critic": 0.02,
-        "alpha": 0.01,
-        "drebrac": {
-            "alpha_actor": 0.01,
-            "alpha_critic": 0.02
-        }
-    },
-    "door-human-v1": {
-        "fql": {
-            "alpha_actor": 30000.0
-        },
-        "trigflow": {
-            "alpha_actor": 30000.0
-        },
-        "rebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.1
-        },
-        "alpha": 0.1,
-        "drebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.1
-        }
-    },
-    "door-cloned-v1": {
-        "fql": {
-            "alpha_actor": 30000.0
-        },
-        "trigflow": {
-            "alpha_actor": 30000.0
-        },
-        "rebrac": {
-            "alpha_actor": 0.01,
-            "alpha_critic": 0.1
-        },
-        "alpha": 0.01,
-        "drebrac": {
-            "alpha_actor": 0.01,
-            "alpha_critic": 0.1
-        }
+        "alpha": 10000.0,
     },
     "door-expert-v1": {
-        "alpha_actor": 30000.0,
-        "rebrac": {
-            "alpha_actor": 0.05,
-            "alpha_critic": 0.01
-        },
-        "alpha": 0.05,
-        "drebrac": {
-            "alpha_actor": 0.05,
-            "alpha_critic": 0.01
-        }
-    },
-    "hammer-human-v1": {
-        "fql": {
-            "alpha_actor": 30000.0
-        },
-        "trigflow": {
-            "alpha_actor": 30000.0
-        },
-        "rebrac": {
-            "alpha_actor": 0.01,
-            "alpha_critic": 0.5
-        },
-        "alpha": 0.01,
-        "drebrac": {
-            "alpha_actor": 0.01,
-            "alpha_critic": 0.5
-        }
-    },
-    "hammer-cloned-v1": {
-        "fql": {
-            "alpha_actor": 10000.0
-        },
-        "trigflow": {
-            "alpha_actor": 10000.0
-        },
-        "rebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.5
-        },
-        "alpha": 0.1,
-        "drebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.5
-        }
+        "alpha": 10000.0,
     },
     "hammer-expert-v1": {
-        "alpha_actor": 30000.0,
-        "rebrac": {
-            "alpha_actor": 0.01,
-            "alpha_critic": 0.01
-        },
-        "alpha": 0.01,
-        "drebrac": {
-            "alpha_actor": 0.01,
-            "alpha_critic": 0.01
-        }
-    },
-    "relocate-human-v1": {
-        "fql": {
-            "alpha_actor": 10000.0
-        },
-        "trigflow": {
-            "alpha_actor": 10000.0
-        },
-        "rebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.01
-        },
-        "alpha": 0.1,
-        "drebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.01
-        }
-    },
-    "relocate-cloned-v1": {
-        "fql": {
-            "alpha_actor": 30000.0
-        },
-        "trigflow": {
-            "alpha_actor": 30000.0
-        },
-        "rebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.01
-        },
-        "alpha": 0.1,
-        "drebrac": {
-            "alpha_actor": 0.1,
-            "alpha_critic": 0.01
-        }
+        "alpha": 3000.0,
     },
     "relocate-expert-v1": {
-        "alpha_actor": 30000.0,
-        "rebrac": {
-            "alpha_actor": 0.05,
-            "alpha_critic": 0.01
-        },
-        "alpha": 0.05,
-        "drebrac": {
-            "alpha_actor": 0.05,
-            "alpha_critic": 0.01
-        }
+        "alpha": 100000.0,
     },
-    "visual-cube-single-play-singletask-v0": {
-        "iql": {
-            "alpha_actor": 1.0
-        },
-        "diql": {
-            "alpha_actor": 1.0
-        },
-        "fql": {
-            "alpha_actor": 300.0
-        },
-        "trigflow": {
-            "alpha_actor": 300.0
-        }
-    },
-    "visual-cube-double-play-singletask-v0": {
-        "iql": {
-            "alpha_actor": 0.3
-        },
-        "diql": {
-            "alpha_actor": 0.3
-        },
-        "fql": {
-            "alpha_actor": 100.0
-        },
-        "trigflow": {
-            "alpha_actor": 100.0
-        }
-    },
-    "visual-scene-play-singletask-v0": {
-        "iql": {
-            "alpha_actor": 10.0
-        },
-        "diql": {
-            "alpha_actor": 10.0
-        },
-        "fql": {
-            "alpha_actor": 100.0
-        },
-        "trigflow": {
-            "alpha_actor": 100.0
-        }
-    },
-    "visual-puzzle-3x3-play-singletask-v0": {
-        "iql": {
-            "alpha_actor": 10.0
-        },
-        "diql": {
-            "alpha_actor": 10.0
-        },
-        "fql": {
-            "alpha_actor": 300.0
-        },
-        "trigflow": {
-            "alpha_actor": 300.0
-        }
-    },
-    "visual-puzzle-4x4-play-singletask-v0": {
-        "iql": {
-            "alpha_actor": 3.0
-        },
-        "diql": {
-            "alpha_actor": 3.0
-        },
-        "fql": {
-            "alpha_actor": 300.0
-        },
-        "trigflow": {
-            "alpha_actor": 300.0
-        }
-    }
+    # "visual-cube-single-play-singletask-v0": {
+    #     "iql": {
+    #         "alpha_actor": 1.0
+    #     },
+    #     "diql": {
+    #         "alpha_actor": 1.0
+    #     },
+    #     "fql": {
+    #         "alpha_actor": 300.0
+    #     },
+    #     "trigflow": {
+    #         "alpha_actor": 300.0
+    #     }
+    # },
+    # "visual-cube-double-play-singletask-v0": {
+    #     "iql": {
+    #         "alpha_actor": 0.3
+    #     },
+    #     "diql": {
+    #         "alpha_actor": 0.3
+    #     },
+    #     "fql": {
+    #         "alpha_actor": 100.0
+    #     },
+    #     "trigflow": {
+    #         "alpha_actor": 100.0
+    #     }
+    # },
+    # "visual-scene-play-singletask-v0": {
+    #     "iql": {
+    #         "alpha_actor": 10.0
+    #     },
+    #     "diql": {
+    #         "alpha_actor": 10.0
+    #     },
+    #     "fql": {
+    #         "alpha_actor": 100.0
+    #     },
+    #     "trigflow": {
+    #         "alpha_actor": 100.0
+    #     }
+    # },
+    # "visual-puzzle-3x3-play-singletask-v0": {
+    #     "iql": {
+    #         "alpha_actor": 10.0
+    #     },
+    #     "diql": {
+    #         "alpha_actor": 10.0
+    #     },
+    #     "fql": {
+    #         "alpha_actor": 300.0
+    #     },
+    #     "trigflow": {
+    #         "alpha_actor": 300.0
+    #     }
+    # },
+    # "visual-puzzle-4x4-play-singletask-v0": {
+    #     "iql": {
+    #         "alpha_actor": 3.0
+    #     },
+    #     "diql": {
+    #         "alpha_actor": 3.0
+    #     },
+    #     "fql": {
+    #         "alpha_actor": 300.0
+    #     },
+    #     "trigflow": {
+    #         "alpha_actor": 300.0
+    #     }
+    # }
 }
