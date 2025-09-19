a2efd172abc9009f406d9eb52a9ad3b0b8ea3472
chunchuanlyu <chunchuan.lv@gmail.com>
2025-09-19 09:23:26 +0800

    alphas

--- scripts/run_alphas.sh
+++ scripts/run_alphas.sh
@@ -5,7 +5,7 @@
 # to be passed as arguments.
 
 # Check for the correct number of arguments
-if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+if [ "$#" -lt 2 ] ; then
     echo "Usage: $0 <agent_name> <env_name> [exp_name]"
     echo "Example: $0 my_new_agent my_new_environment-v1 my_experiment"
     exit 1
@@ -15,10 +15,6 @@ fi
 AGENT_NAME=$1
 ENV_NAME=$2
 
-EXP_NAME=""
-if [ "$#" -eq 3 ]; then
-    EXP_NAME=$3
-fi
 
 # pen-expert-v1		
 # door-expert-v1		
@@ -26,15 +22,15 @@ fi
 # relocate-expert-v1	
 
 # Define the list of alpha parameters
-alphas=(10 30.0 100.0 3000.0  )
+alphas=(1 3  10 )
 
 # Loop through all alpha values
 for alpha in "${alphas[@]}"; do
-    echo "Running with Agent: $AGENT_NAME, Env: $ENV_NAME, Alpha: $alpha, ExpName: $EXP_NAME"
+    echo "Running with Agent: $AGENT_NAME, Env: $ENV_NAME, Alpha: $alpha, ExpName: "
     python main.py \
         --agent "agents/$AGENT_NAME.py" \
         --env_name "$ENV_NAME" \
         --alpha_actor "$alpha" \
-        --exp_name alpha_actor \
+        --exp_name alpha_actor $3 $4 $5 $6 $7\
         --seed "$RANDOM" 
 done
