e3ce2cba90d742016cd052a886ce97b6ff499e94
chunchuanlyu <chunchuan.lv@gmail.com>
2025-10-06 10:54:34 +0800

    og all

--- scripts/run_OG.sh
+++ scripts/run_OG.sh
@@ -14,28 +14,23 @@ fi
 # Assign command-line arguments to variables for clarity
 AGENT_NAME=$1
 
-# Check if a seed is provided as the second argument
-if [ -n "$2" ] ; then
-    seed=$2
-    # Shift arguments to handle the rest of the optional parameters
-    shift 2
-else
-    seed=$RANDOM
-    shift 1
-fi
+
 
 # Define the list of environment names
 env_names=("antmaze-large-navigate-singletask-v0" 'humanoidmaze-medium-navigate-singletask-v0' "antsoccer-arena-navigate-singletask-v0" "cube-single-play-singletask-v0" "scene-play-singletask-v0"  'humanoidmaze-large-navigate-singletask-v0' "cube-double-play-singletask-v0" "puzzle-3x3-play-singletask-v0" "puzzle-4x4-play-singletask-v0" )
+seeds=(111 222 333 444 555 666 777 888)
 #env_names=("puzzle-3x3-play-singletask-v0" )
 
 # Loop through all environment names
-for env_name in "${env_names[@]}"; do
-    echo "Running with Agent: $AGENT_NAME, Env: $env_name, Seed: $seed, Additional Args: $@"
-    python main.py \
-        --agent "agents/$AGENT_NAME.py" \
-        --env_name "$env_name" \
-        --run_group submit_final \
-            --noretest \
-        --seed "$seed" \
-        "$@"
+for seed in "${seeds[@]}"; do
+    for env_name in "${env_names[@]}"; do
+        echo "Running with Agent: $AGENT_NAME, Env: $env_name, Seed: $seed, Additional Args: $@"
+        python main.py \
+            --agent "agents/$AGENT_NAME.py" \
+            --env_name "$env_name" \
+            --run_group submit_OG \
+                --noretest \
+            --seed "$seed" \
+            "$@"
+    done
 done
