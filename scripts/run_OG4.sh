bb9dff5c70c458f0fe4ee1575208bfd11f780be6
chunchuanlyu <chunchuan.lv@gmail.com>
2025-11-14 21:10:53 +0800

    dmfql target 1

--- scripts/run_OG4.sh
+++ scripts/run_OG4.sh
@@ -33,7 +33,7 @@ for seed in "${seeds[@]}"; do
         python main.py \
             --agent "agents/$AGENT_NAME.py" \
             --env_name "$env_name" \
-            --run_group submit_OG_v2 \
+            --run_group submit_OG_target1 \
                 --noretest \
             --seed "$seed" \
             "$@"
